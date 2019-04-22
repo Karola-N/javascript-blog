8ce340dd93e100e5ae8b78c30688295b0adbe9c8
Karolina Nowak <nowak.karolina90@gmail.com>
2019-04-22 16:09:39 +0200

    Add class active to clicked link (probably)

--- js/script.js
+++ js/script.js
@@ -4,26 +4,36 @@ function titleClickHandler(event) {
     console.log('Link was clicked!');
     console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
-
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.post.active');
-
-    for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    const clickedElement = this,
+        links = document.querySelectorAll('.titles a');
+    console.log('clickedElement:', clickedElement);
+    /* alternative version of the above:
+    console.log('clickedElement (with plus): ' + clickedElement); */
+    for (let link of links) {
+        clickedElement.classList.add('active');
+        //link.addEventListener('click', clickedElement.classList.add('active'));
+        //link.classList.add('active');
     }
-    /* get 'href' attribute from the clicked link */
-
-    /* find the correct article using the selector (value of 'href' attribute) */
-
-    /* add class 'active' to the correct article */
 }
+
+/* [DONE] remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll('.post.active');
+
+for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+}
+/* get 'href' attribute from the clicked link */
+
+/* find the correct article using the selector (value of 'href' attribute) */
+
+/* add class 'active' to the correct article */
 
 const links = document.querySelectorAll('.titles a');
 
